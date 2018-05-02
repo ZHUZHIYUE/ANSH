842aee06c516bb1d1178377acca822f6d574ad4c
zhuxi <86688081@qq.com>
2018-05-02 17:09:52 +0800

    feat(ANSH.OAuth): v.1.1.0
    增加OAuth授权操作类库

---
+++ ANSH.OAuth/Token.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ANSH.OAuth
+{
+
+    /// <summary>
+    /// 令牌
+    /// </summary>
+    public class Token
+    {
+
+        /// <summary>
+        /// 令牌值
+        /// </summary>
+        public string Access_Token
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// 令牌值有效时间（单位分）
+        /// </summary>
+        public int Expires_IN
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime Create_Times
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 刷新令牌值
+        /// </summary>
+        public string Refresh_Token
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 刷新令牌有效时间（单位分）
+        /// </summary>
+        public int Refexpires_IN
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ ANSH.OAuth/TokenCode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ANSH.OAuth
+{
+    /// <summary>
+    /// 授权码
+    /// </summary>
+    public class TokenCode
+    {
+        /// <summary>
+        /// 授权码值
+        /// </summary>
+        public string Code
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 授权码值有效时间（单位分）
+        /// </summary>
+        public int Expires_IN
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime Create_Times
+        {
+            get;
+            set;
+        }
+    }
+}
