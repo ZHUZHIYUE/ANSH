16b779641a41032109cfc3512b0ec2793a330d9e
zhuxi <86688081@qq.com>
2018-05-02 17:12:22 +0800

    feat(ANSH.API): v1.1.0
    增加对WebAPI SDK操作（请求-响应）基类

---
+++ ANSH.API/ResponseContracts/BaseResponse.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+
+namespace ANSH.API.ResponseContracts
+{
+    /// <summary>
+    /// 响应
+    /// </summary>
+    public abstract class BaseResponse
+    {
+        int _error = 0;
+        /// <summary>
+        /// 返回码
+        /// </summary>
+        public int error
+        {
+            get { return _error; }
+            set { _error = value; }
+        }
+
+
+        string _msg = "SUCCESS";
+        /// <summary>
+        /// 错误消息
+        /// </summary>
+        public string msg
+        {
+            get { return _msg; }
+            set { _msg = value; }
+        }
+
+
+        string _result = "SUCCESS";
+        /// <summary>
+        /// 返回信息
+        /// </summary>
+        /// <returns></returns>
+        public string result
+        {
+            get { return _result; }
+            set { _result = value; }
+        }
+    }
+}
