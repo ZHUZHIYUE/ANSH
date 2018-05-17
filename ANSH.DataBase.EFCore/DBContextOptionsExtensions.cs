1fd38ea07664a603e092730a1dc77354c16c220e
zhuxi <86688081@qq.com>
2018-05-17 09:14:28 +0800

    feat(ANSH.DataBase.EFCore): 增加参数hasnext，是否还有下一页

--- ANSH.DataBase.EFCore/DBContextOptionsExtensions.cs
+++ ANSH.DataBase.EFCore/DBContextOptionsExtensions.cs
@@ -6,8 +6,7 @@ using ANSH.DataBase.EFCore;
 /// <summary>
 /// 提供DBContext操作扩展方法
 /// </summary>
-public static class DBContextOptionsExtensions
-{
+public static class DBContextOptionsExtensions {
     /// <summary>
     /// 将指定的查询结果进行分页处理
     /// <para>注意：当需要对查询结果进行排序时，应先排序再进行分页</para>
@@ -15,14 +14,15 @@ public static class DBContextOptionsExtensions
     /// <param name="iqueryable">将指定的查询结果</param>
     /// <param name="datacount">满足指定条件数据总条数</param>
     /// <param name="pagecount">满足指定条件数据可分页总数</param>
+    /// <param name="hasnext">是否还有下一页</param>
     /// <param name="page">页数</param>
     /// <param name="pagesize">每页数据条数</param>
     /// <returns>返回分页处理后的查询结果</returns>
-    public static IQueryable<TEntity> ToPage<TEntity>(this IQueryable<TEntity> iqueryable, out int datacount, out int pagecount, int page = 1, int pagesize = 20) where TEntity : DBEntity
-    {
-        datacount = iqueryable.Count();
-        pagecount = (int)Math.Ceiling(datacount / (double)pagesize);
-        return iqueryable.Skip((page - 1) * pagesize).Take(pagesize);
+    public static IQueryable<TEntity> ToPage<TEntity> (this IQueryable<TEntity> iqueryable, out int datacount, out int pagecount, out bool hasnext, int page = 1, int pagesize = 20) where TEntity : DBEntity {
+        datacount = iqueryable.Count ();
+        pagecount = (int) Math.Ceiling (datacount / (double) pagesize);
+        hasnext = page < pagecount;
+        return iqueryable.Skip ((page - 1) * pagesize).Take (pagesize);
     }
 
     /// <summary>
@@ -32,17 +32,15 @@ public static class DBContextOptionsExtensions
     /// <param name="left">第一个操作</param>
     /// <param name="right">第二个操作</param>
     /// <returns>一个 System.Linq.Expressions.Expression</returns>
-    public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right)
-    {
-        if (left == null)
-        {
+    public static Expression<Func<T, bool>> And<T> (this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right) {
+        if (left == null) {
             return right;
         }
-        var param = Expression.Parameter(typeof(T));
+        var param = Expression.Parameter (typeof (T));
 
-        var expr = Expression.AndAlso(Expression.Invoke(left, param), Expression.Invoke(right, param));
+        var expr = Expression.AndAlso (Expression.Invoke (left, param), Expression.Invoke (right, param));
 
-        return Expression.Lambda<Func<T, bool>>(expr, param);
+        return Expression.Lambda<Func<T, bool>> (expr, param);
     }
 
     /// <summary>
@@ -52,17 +50,15 @@ public static class DBContextOptionsExtensions
     /// <param name="left">第一个操作</param>
     /// <param name="right">第二个操作</param>
     /// <returns>一个 System.Linq.Expressions.Expression</returns>
-    public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right)
-    {
-        if (left == null)
-        {
+    public static Expression<Func<T, bool>> Or<T> (this Expression<Func<T, bool>> left, Expression<Func<T, bool>> right) {
+        if (left == null) {
             return right;
         }
 
-        var param = Expression.Parameter(typeof(T));
+        var param = Expression.Parameter (typeof (T));
 
-        var expr = Expression.OrElse(Expression.Invoke(left, param), Expression.Invoke(right, param));
+        var expr = Expression.OrElse (Expression.Invoke (left, param), Expression.Invoke (right, param));
 
-        return Expression.Lambda<Func<T, bool>>(expr, param);
+        return Expression.Lambda<Func<T, bool>> (expr, param);
     }
 }
